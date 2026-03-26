0c371a0e0315dc22fe0f8660cdbebbcb14999c5f
Matias Jara <mati.jarag@gmail.com>
2026-03-25 21:53:13 -0400

    clear project

--- README.md
+++
@@ -1,2 +0,0 @@
-# Bienvenid@ a mi página web
-Es una página web estática, ya que es una página hosteada en github y para eso sólo se puede utilizar html,css,js.
--- logicaNombre.js
+++
@@ -1,6 +0,0 @@
-/* ENVIAR NOMBRE A PAGINA PRINCIPAL */
-const btnLogin = document.querySelector(".botonLogin");
-btnLogin.addEventListener("click", () => {
-    const valueLogin = document.getElementById("nombreUsu");
-    localStorage.setItem("nombreInvitado",valueLogin.value);
-});
